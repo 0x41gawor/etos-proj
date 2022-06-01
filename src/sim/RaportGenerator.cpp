cc7dd817386d44794ddd6d235f67e6a6d26bc6f2
0x41gawor <41gawor@gmail.com>
2022-06-01 20:20:01 +0200

    feat(Sim): implement stats #24

--- src/sim/RaportGenerator.cpp
+++ src/sim/RaportGenerator.cpp
@@ -13,7 +13,7 @@ void RaportGenerator::run(Stats stats, System::System system,  double simTime)
 	// dolicz ostatnie u
 	stats.u(simTime, system.server.status);
 	// wyswietl wyniki
-	std::cout << "Sredni czas oczekiwania w kolejce: " << stats.d_mean << "\n";
-	std::cout << "Liczba klientow w kolejce oszacowana w czasie ciaglym: " << stats.q_value << "\n";
+	std::cout << "Sredni czas oczekiwania w kolejce: " << stats.d_mean[0] << "\n";
+	std::cout << "Liczba klientow w kolejce oszacowana w czasie ciaglym: " << stats.q_value[0] << "\n";
 	std::cout << "Wykorzystanie serwera obslugi: " << stats.u_value << "\n";
 }
--- src/sim/Stats.h
+++ src/sim/Stats.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../system/ServerStatusEnum.h"
+#include "../system/FlowEnum.h"
 
 #include <ostream>
 #include <iostream>
@@ -10,21 +11,23 @@ namespace Sim
 	class Stats
 	{
 	public:																			// < M E M B E R S >
-		double d_accumulated;
-		double d_number;
-		double d_mean;
+		double d_accumulated[3];
+		double d_number[3];
+		double d_mean[3];
 
-		double q_rects;
-		double q_lastSimTime;
-		double q_value;
+		double q_rects[3];
+		double q_lastSimTime[3];
+		double q_value[3];
 
 		double u_rects;
 		double u_lastSimTime;
 		double u_value;
 	public:																			// < M E T H O D S >
 		Stats();
-		void d(double delay); 
-		void q(double simTime, int); 
+		void d(double delay);														// overall delay
+		void d(double delay, System::FlowEnum flow);								// delay for specific flow
+		void q(double simTime, int);												// overall queue.count
+		void q(double simTime, int, System::FlowEnum flow);							// queue.count for specific flow	
 		void u(double simTime, System::ServerStatusEnum status); 
 	};
 
